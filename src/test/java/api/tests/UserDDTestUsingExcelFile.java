735e2f3641353b2b4c7f2deb010c929fc156cd2c
Mihir992 <119054373+Mihir992@users.noreply.github.com>
2024-03-25 09:46:53 +0530

    Update UserDDTestUsingExcelFile.java
    Implement reading user daga form excel using dataprovider

--- src/test/java/api/tests/UserDDTestUsingExcelFile.java
+++ src/test/java/api/tests/UserDDTestUsingExcelFile.java
@@ -1,10 +1,9 @@
 package api.tests;
 
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import api.endpoints.UserEndPoints;
 import api.payload.User;
 import api.utils.ExcelUtils;
 import io.restassured.response.Response;
@@ -12,19 +11,15 @@ import io.restassured.response.Response;
 public class UserDDTestUsingExcelFile {
 	
 	User userPayload;
-	public Logger logger; // for logs
-
+	public Logger logger; 
   
-	@Test(priority=1, dataProvider="AllUserData", dataProviderClass=ExcelUtils.class )
-	public void testGetUser()
-	{
+	@Test(dataProvider = "AllUserData", dataProviderClass = ExcelUtils.class)
+    public void testGetUser(String uname, String password) {
+		logger= LogManager.getLogger(this.getClass());
+		logger.debug("debugging.....");
 		logger.info("********** Reading User Info ***************");
-		
-		Response response=UserEndPoints.readUser(this.userPayload.getUsername());
-		response.then().log().all();
-		Assert.assertEquals(response.getStatusCode(),200);
-		
-		logger.info("**********User info  is displayed ***************");
-		
-	}
+		logger.info("Username: " + uname);
+        logger.info("Password: " + password);
+        logger.info("********** User info is displayed ***************");
+    }
 }
